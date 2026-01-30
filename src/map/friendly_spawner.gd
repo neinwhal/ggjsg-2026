646d3686e8f8dce69c3141e1ddc8d00d8ee414fe
neinwhal <3191240+neinwhal@users.noreply.github.com>
2026-01-31 03:40:00 +0800

    add collision to friendlies and spawn stupid friendlies box

---
+++ src/map/friendly_spawner.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var FriendlyScene: PackedScene = preload("res://src/friendly/friendly_basic.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+var spawn_timer := 0.0
+
+func spawn_friendly() -> void:
+	var e := FriendlyScene.instantiate()
+	e.global_position = Vector2(600, 300)
+	add_child(e)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	spawn_timer += delta
+
+	if spawn_timer >= 1.0:
+		spawn_timer = 0.0
+		spawn_friendly()
