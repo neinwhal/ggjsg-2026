1a2604d9e81adb390d7d4bef26d5cbe86728ba27
neinwhal <3191240+neinwhal@users.noreply.github.com>
2026-01-31 14:43:25 +0800

    spawn zombois further away at either sides

--- src/map/enemy_spawner.gd
+++ src/map/enemy_spawner.gd
@@ -9,9 +9,21 @@ func _ready() -> void:
 var spawn_timer := 0.0
 
 func spawn_enemy() -> void:
+	var player := get_tree().get_first_node_in_group("player")
+	if player == null:
+		return
+	
 	var e := EnemyScene.instantiate()
-	e.global_position = Vector2(600, 300)
 	add_child(e)
+	
+	var player_pos: Vector2 = player.global_position
+	var side : float = [-1.0, 1.0].pick_random()
+
+	# X relative to player, Y fixed
+	e.global_position = Vector2(
+		player_pos.x + 1000.0 * side,
+		300.0
+	)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
