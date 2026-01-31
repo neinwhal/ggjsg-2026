51c4bd4079e80232037fcb37b64b7df181d82a71
katlong0709 <katlong0709@gmail.com>
2026-01-31 16:41:50 +0800

    added start and end sections

--- src/level/main_level.gd
+++ src/level/main_level.gd
@@ -5,9 +5,12 @@ var rand: RandomNumberGenerator
 
 #const SECTION_WIDTH: int = 2304 #x2 viewport x
 const SECTION_WIDTH: int = 1728 # x1.5 viewport x
-const VIEWPORT_SIZE: int = 1152
-var current_min_x: int = -SECTION_WIDTH
+#const VIEWPORT_SIZE: int = 1152
+const MAX_MID_SECTIONS: int = 3
 var current_max_x: int = SECTION_WIDTH
+var mid_section_count: int = 0
+var is_end_generated: bool = false
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,11 +23,9 @@ func _ready() -> void:
 	rand = RandomNumberGenerator.new()
 	rand.randomize()
 	
-	generate_random_section(0)
-	generate_random_section(-SECTION_WIDTH)
+	generate_start_section()
 	generate_random_section(SECTION_WIDTH)
 	#update_section_around_player()
-	$LeftExtent.position.x = -SECTION_WIDTH
 	$RightExtent.position.x = SECTION_WIDTH
 
 
@@ -32,24 +33,18 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
-func update_section_around_player() -> void:
-	## Triggered when player hits world extent
-	if $PlayerBasic.position.x < 0:
-		## Spawn section to left
-		current_min_x -= SECTION_WIDTH
-		$LeftExtent.position.x -= SECTION_WIDTH
-		generate_random_section(current_min_x)
-		#print_debug("Spawning left")
-	elif $PlayerBasic.position.x > 0:
-		## Spawn section to right
-		current_max_x += SECTION_WIDTH
-		$RightExtent.position.x += SECTION_WIDTH
-		generate_random_section(current_max_x)
-		#print_debug("Spawning right")
-	else:
-		## Shouldnt happen
-		print_debug("Player xpos is 0 when hitting world boundary!!!")
-		pass
+func generate_start_section(xpos: int = 0) -> void:
+	var start_scene := preload("res://src/level/start_section.tscn").instantiate()
+	start_scene.position.x = 0
+	call_deferred("add_child", start_scene)
+
+func generate_end_section(xpos: int) -> void:
+	if is_end_generated:
+		return
+	is_end_generated = true
+	var end_scene := preload("res://src/level/end_section.tscn").instantiate()
+	end_scene.position.x = xpos
+	call_deferred("add_child", end_scene)
 
 ## Obtain random scene from container
 func generate_random_section(xpos: int) -> void:
@@ -58,15 +53,17 @@ func generate_random_section(xpos: int) -> void:
 		new_scene.position.x = xpos
 		#add_child(new_scene)
 		call_deferred("add_child", new_scene)
+		mid_section_count += 1
 	else:
 		print_debug("Trying to obtain scene from empty scene selection container!!!")
 
-
-func _on_left_extent_body_entered(body: Node2D) -> void:
-	print_debug("Hit left extent!!!")
-	update_section_around_player()
-
-
 func _on_right_extent_body_entered(body: Node2D) -> void:
-	print_debug("Hit right extent!!!")
-	update_section_around_player()
+	#print_debug("Hit right extent!!!")
+	## Spawn section to right
+	if mid_section_count == MAX_MID_SECTIONS:
+		current_max_x += SECTION_WIDTH
+		generate_end_section(current_max_x)
+	else:
+		current_max_x += SECTION_WIDTH
+		$RightExtent.position.x += SECTION_WIDTH
+		generate_random_section(current_max_x)
