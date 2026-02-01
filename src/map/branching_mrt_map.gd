1a73dff4aef7dc1cf5ca75d6eca69daf1669d308
katlong0709 <katlong0709@gmail.com>
2026-02-01 14:36:25 +0800

    added node blink logic

--- src/map/branching_mrt_map.gd
+++ src/map/branching_mrt_map.gd
@@ -8,13 +8,63 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hide()
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func _on_node_11_pressed() -> void:
+	pass # Replace with function body.
 
-#func _on_go_level_2_pressed() -> void:
-	#hide()
-	#_on_lvl_2_button_pressed.emit()
+
+func _on_node_21_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_22_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_31_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_32_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_33_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_41_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_42_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_51_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_52_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_53_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_61_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_62_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_node_71_pressed() -> void:
+	pass # Replace with function body.
---
+++ src/map/node_logic.gd
@@ -0,0 +1,65 @@
+extends TextureButton
+
+## Animation State
+enum NodeAnimation {
+	UNLIGHTED,		# For current node and non neighbouring nodes
+	BLINKING,		# For neighbouring nodes
+	CLICKED			# For neighbouring nodes upon click
+}
+
+const NODE_FRAMES: int = 5
+const NODE_FRAME_WIDTH: int = 16
+const NODE_SPRITESHEET_WIDTH: int = 80
+const IDEAL_NODE_FPS: int = 5
+
+var duration_per_frame: float = 1.0 / IDEAL_NODE_FPS
+var node_duration_count: float
+var animation_state: NodeAnimation
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_state = NodeAnimation.UNLIGHTED
+	animation_state = NodeAnimation.BLINKING
+	var atlas_normal: AtlasTexture = texture_normal
+	atlas_normal.region.size.x = 16
+	atlas_normal.region.size.y = 16
+	atlas_normal.region.position.x = 0
+	atlas_normal.region.position.y = 0
+	
+	node_duration_count = 0.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	match animation_state:
+		NodeAnimation.UNLIGHTED:
+			node_duration_count = 0.0
+			var atlas_normal: AtlasTexture = texture_normal as AtlasTexture
+			atlas_normal.region.position.x = 0
+		NodeAnimation.BLINKING:
+			node_duration_count += delta
+			if node_duration_count >= duration_per_frame:
+				node_duration_count -= duration_per_frame
+				# Change to next frame
+				var atlas_normal: AtlasTexture = texture_normal as AtlasTexture
+				#print_debug("pos_x: ", atlas.region.position.x)
+				#print(texture.get_size().x)
+				if (atlas_normal.region.position.x + NODE_FRAME_WIDTH) < NODE_SPRITESHEET_WIDTH:
+					atlas_normal.region.position.x += NODE_FRAME_WIDTH
+					#print_debug("+ width")
+				else:
+					atlas_normal.region.position.x = 0
+		NodeAnimation.CLICKED:
+			node_duration_count = 0.0
+		_:
+			pass
+			print_debug("Node in invalid state!!!")
+
+
+func set_unlighted() -> void:
+	animation_state = NodeAnimation.UNLIGHTED
+
+func set_blinking() -> void:
+	animation_state = NodeAnimation.BLINKING
+
+func set_clicked() -> void:
+	animation_state = NodeAnimation.CLICKED
