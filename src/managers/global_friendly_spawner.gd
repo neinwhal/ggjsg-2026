e1b51d825f2f37fcf1887acbbfaf3fe47d6fbde1
neinwhal <3191240+neinwhal@users.noreply.github.com>
2026-02-01 18:59:45 +0800

    CHEAT SPAWN

--- src/managers/global_friendly_spawner.gd
+++ src/managers/global_friendly_spawner.gd
@@ -10,6 +10,16 @@ extends Node2D
 @onready var FriendlyRanged: PackedScene = preload("res://src/friendly/friendly_ranged.tscn")
 @onready var FriendlyRanged2: PackedScene = preload("res://src/friendly/friendly_ranged_2.tscn")
 
+@onready var friendly_key_map := {
+	KEY_1: FriendlyMelee,
+	KEY_2: FriendlyMelee2,
+	KEY_3: FriendlyRanged,
+	KEY_4: FriendlyRanged2,
+	KEY_5: FriendlyTank,
+	KEY_6: FriendlyBianlian,
+	KEY_7: FriendlyRanged2,
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -18,43 +28,30 @@ var spawn_timer := 0.0
 var spawn_count : int = 0
 var max_count : int = 1
 
-func spawn_friendly() -> void:
+func spawn_friendly(friendly_scene: PackedScene) -> void:
 	var player := get_tree().get_first_node_in_group("player")
 	if player == null:
 		return
-	
-	spawn_count = spawn_count + 1
-	#var e := FriendlyTank.instantiate()
-	#var e2 := FriendlyRanged.instantiate()
-	var e3 := FriendlyBianlian.instantiate()
-	#var e4 := FriendlyRanged2.instantiate()
-	#e.global_position = Vector2(player.global_position.x + randf_range(-250.0, 250.0), -500.0)
-	#e2.global_position = Vector2(player.global_position.x + randf_range(-250.0, 250.0), -500.0)
-	e3.global_position = Vector2(player.global_position.x + randf_range(-250.0, 250.0), -500.0)
-	#e4.global_position = Vector2(player.global_position.x + randf_range(-250.0, 250.0), -500.0)
-	#add_child(e)
-	#add_child(e2)
-	add_child(e3)
-	#add_child(e4)
+
+	spawn_count += 1
+
+	var e := friendly_scene.instantiate() as Node2D
+	add_child(e)
+
+	e.global_position = Vector2(
+		player.global_position.x + randf_range(-250.0, 250.0),
+		-500.0
+	)
 
 
 var temp_spawning_stop : bool = false;
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
-	#if (spawn_count < max_count):
-		#spawn_timer += delta
-		#
-		#if spawn_timer >= 1.0:
-			#spawn_timer = 0.0
-			#spawn_friendly()
-		
-	#### CHEAT SPAWNS
-	if Input.is_key_pressed(KEY_X):
-		if (!temp_spawning_stop):
-			temp_spawning_stop = true
-			spawn_friendly()
-	else:
-		if (temp_spawning_stop):
-			temp_spawning_stop = false
-		
+	for key in friendly_key_map.keys():
+		if Input.is_key_pressed(key):
+			if not temp_spawning_stop:
+				temp_spawning_stop = true
+				spawn_friendly(friendly_key_map[key])
+			return
+	# reset latch when no key is held
+	temp_spawning_stop = false
